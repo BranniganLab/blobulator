a41ad621934e5139ceee1800f9da33eb31856e32
Ezry <ezrysm@gmail.com>
2022-01-21 14:23:46 -0500

    Zoomable hydropathy plot complete

--- static/figure.js
+++ static/figure.js
@@ -1,3 +1,365 @@
+class ZFigure {
+	constructor(figID, data){
+		// These constants set fixed values for height and width to be used in making all visualizations
+		this.MARGIN = { top: 30, right: 230, bottom: 30, left: 50 };
+		this.WIDTH = 1200 - this.MARGIN.left - this.MARGIN.right;
+		this.HEIGHT = 200 - this.MARGIN.top - this.MARGIN.bottom;
+		
+		this.figID = figID
+		this.data = data
+		
+		let node = document.createElement("div");
+		node.style.position = "relative";
+		this.container = document.getElementById("my_dataviz").appendChild(node);		
+		
+		// append the svg object to the body of the page
+		this.svg = d3.select(this.container)
+		  .append("svg")
+			.attr("width", this.WIDTH + this.MARGIN.left + this.MARGIN.right)
+			.attr("height", this.HEIGHT + this.MARGIN.top + this.MARGIN.bottom + 45)
+		  .append("g")
+			.attr("transform",
+				  "translate(" + this.MARGIN.left + "," + this.MARGIN.top + ")");
+		
+		return this
+	}
+	
+	add_title(title){
+	    // Creates the title
+		this.svg.append("text")
+			.attr("x", this.WIDTH / 2)
+			.attr("y", this.MARGIN.top - 25)
+			.style("text-anchor", "middle")
+			.text(title)
+			.attr("font-size", "20px")
+
+		return this;
+	}
+	
+	/* add_tooltip
+		FUNCTION: add_tooltip
+		SHORT DESCRIPTION: add a small i that provides information to the user in a tooltip
+		INPUTS:
+			svg - a container for a graph (modified by the function directly)
+		RETURNS:
+			none
+	*/
+	add_tooltip(content="Place Holder", xpos=this.WIDTH, ypos=this.MARGIN.top-20) {
+		this.infoIcon = document.createElement("div");
+		this.infoIcon.style.position = "absolute";
+		this.infoIcon.style.top = ypos + "px";
+		this.infoIcon.style.left = xpos + 30 + "px";
+		this.infoIcon.style.font = "arial";
+		this.infoIcon.style.cursor = "pointer";
+		this.infoIcon.style.fontSize = "larger";
+		this.infoIcon.style.fill = "blue";
+		this.infoIcon.innerText = '\u{24D8}';
+		this.infoIcon.type = "button";
+		this.infoIcon.title = '<a onclick="$(this).closest(\'div.popover\').popover(\'hide\');" type="button" class="close" aria-hidden="true">&times;</a><br>';
+		this.infoIcon.style.zIndex = "10"; // Put this element on top of the SVG
+		
+		$(this.infoIcon).popover({
+			content: content, 
+			placement: "top", 
+			html: true,
+			sanitize: false,
+			container: 'body'
+		});
+
+		this.container.appendChild(this.infoIcon);
+
+		return this;
+	}
+	
+}
+
+// Based on this tutorial: https://www.d3-graph-gallery.com/graph/interactivity_zoom.html
+class ZChart extends ZFigure{
+	constructor(figID, data, my_snps, seq, snp_tooltips) {
+		super(figID, data)
+
+		this.y = d3.scaleLinear()
+			.domain([0, 1])
+			.range([this.HEIGHT, 0]);
+			
+		// Add X axis
+		this.x = d3.scaleBand()
+			.range([0, this.WIDTH])
+			.domain(data.map(d => d.resid ))
+			.padding(0.2);
+		var x = this.x
+		
+		if(my_snps == 0){
+			var snps = false
+		}else{
+			var snps = true
+		}
+		this.add_xAxis(snps, x)
+
+
+
+		// Add a clipPath: everything out of this area won't be drawn.
+		var clip = this.svg.append("defs").append("svg:clipPath")
+			.attr("id", "clip")
+			.append("svg:rect")
+			.attr("width", this.WIDTH )
+			.attr("height", this.HEIGHT+45 )
+			.attr("x", 0)
+			.attr("y", 0);
+
+		// Create the plot
+		this.plot = this.svg.append('g')
+			.attr("clip-path", "url(#clip)");
+
+		this.data = data
+
+		this.bars = this.plot.selectAll("bars")
+			.data(this.data)
+			.join("rect")
+			.attr("id", "barChart"+this.figID)
+			.attr("width", x.bandwidth())
+			.attr("x", (d) => x(d.resid))
+			.attr("y", d => this.HEIGHT);
+			
+		if (snps) {
+			this.add_snps(my_snps, seq, snp_tooltips, x)
+			this.update_snps(x)
+		}
+		
+		// Add brushing
+		var brush = d3.brushX()                 // Add the brush feature using the d3.brush function
+			.extent( [ [0,0], [this.WIDTH, this.HEIGHT] ] ) // initialise the brush area: start at 0,0 and finishes at width,height: it means I select the whole graph area
+			.on("end", updateChart) // Each time the brush selection changes, trigger the 'updateChart' function
+		this.plot
+			.append("g")
+			  .attr("class", "brush")
+			  .call(brush);
+
+		// A function that sets idleTimeOut to null
+		var idleTimeout
+		function idled() { idleTimeout = null; }
+		
+		
+		
+		// A function that updates the chart for given boundaries
+		// Make "this." variables local variables so they can be used inside updateChart - clunky
+		var bars = this.bars
+		var plot = this.plot
+		var y = this.y
+		var width = this.WIDTH
+		var svg = this.svg
+		var xAxis = this.xAxis
+		var chart = this
+		const range = ([min, max]) => Array.from({ length: max - min + 1 }, (_, i) => min + i);
+		function updateChart(event) {
+			const extent = event.selection
+			
+			// If no selection, back to initial coordinate. Otherwise, update X axis domain
+			if(!extent){
+			  if (!idleTimeout) return idleTimeout = setTimeout(idled, 350); // This allows to wait a little bit
+			  x.domain(data.map(d => d.resid ))
+			}else{
+			  var domain = [ scaleBandInvert(x)(extent[0]), scaleBandInvert(x)(extent[1]) ]
+			  x.domain(range(domain))
+			  
+			  plot.select(".brush").call(brush.move, null) // This remove the grey brush area as soon as the selection has been done
+			}
+
+			// Update axis and bar position
+			var timing =1000
+			xAxis.transition().duration(timing).call(d3.axisBottom(x))
+			bars
+			  .transition().duration(timing)
+			  .attr("x", function (d) { 
+				if(extent && d.resid>domain[1]){
+					return 2*width
+				}else if(extent && d.resid<domain[0]){
+					return -width;
+				}else{
+					return x(d.resid); 
+				}
+			  })
+			  .attr("y", function (d) { return y(d.hydropathy_3_window_mean); } )
+			  .attr("width", x.bandwidth());
+			chart.update_xAxis(x)
+			if(snps){chart.update_snps(x, extent, domain, width, timing)}
+		}
+		
+		function scaleBandInvert(scale) {
+		  var domain = scale.domain();
+		  var paddingOuter = scale(domain[0]);
+		  var eachBand = scale.step();
+		  return function (value) {
+			var index = Math.floor(((value - paddingOuter) / eachBand));
+			return domain[Math.max(0,Math.min(index, domain.length-1))];
+		  }
+		}
+		
+		return this
+	}
+	
+	add_xAxis(snps, x){
+		if (snps) {
+			var xaxisMargin = this.HEIGHT + 15
+		} else {
+			var xaxisMargin = this.HEIGHT
+		}
+
+		var num_residues = this.data.length
+		
+		this.xAxis = this.svg.append("g")
+						.attr("transform", "translate(0," + xaxisMargin + ")");
+						
+		this.update_xAxis(x)
+		
+		// Bars
+		//Creates the "Residue" x-axis label
+		if (snps) {
+			var bottomMargin = this.MARGIN.bottom + 25
+		} else {
+			var bottomMargin = this.MARGIN.bottom
+		}
+		this.svg.append("text")
+			.attr("x", this.WIDTH / 2)
+			.attr("y", this.HEIGHT + bottomMargin)
+			.style("text-anchor", "middle")
+			.text("Residue")
+		
+		return this
+	}
+	
+	update_xAxis(x){
+		var tickPeriod = (Math.round((Math.round(x.domain().length/10))/10)*10)
+		this.xAxis.call(d3.axisBottom(x).tickValues(x.domain().filter(function(d, i) { return !((i+1) % 
+						   (tickPeriod) )})));
+		return this
+	}
+	
+	/* add_snps
+	*/
+	add_snps(my_snp, my_seq, tooltip_snps, x) {
+		var arc = d3.symbol().type(d3.symbolTriangle);
+		this.snps = this.plot.append('g')
+			.selectAll("rect")
+			.data(my_snp)
+			.enter()
+			.append("path")
+			.attr('d', arc)
+			.attr("fill", 'black')
+			.attr("transform", (d) => "translate(" + (x(d.resid) + x.bandwidth()/2) + ", 145)")
+			.on("click", function(event, d) {
+				//window.location.href = d.xrefs[0].url+'_blank'
+				window.open(d.xrefs.url, '_blank')
+			})
+			.on("mouseover", function(event, d) {
+				d3.select(this)
+					.attr("fill", "red");
+				tooltip_snps.transition()
+					.on("start", () => tooltip_snps.style("display", "block"))
+					.duration(100)
+					.style("opacity", 0.9);
+				tooltip_snps.html(`<a href="${d.xrefs.url}" target="_blank">${d.xrefs.id}</a>, ${my_seq[d.resid-1]}${d.resid}${d.alternativeSequence}`)
+					.style("left", (event.pageX) + 10 + "px")
+					.style("top", (event.pageY - 28) + "px");
+			})
+			.on("mouseout", function(event, d) {
+				d3.select(this).attr("fill", "black");
+				tooltip_snps.transition()
+					.duration(2000)
+					.style("opacity", 0)
+					.on("end", () => tooltip_snps.style("display", "none"));
+			});
+
+		return this;
+	}
+	
+	update_snps(x, extent, domain, width, timing=1000){
+		this.snps.transition()
+			.duration(timing)
+			.attr("transform", function(d){
+				if(extent && d.resid>domain[1]){
+					var translation = ("translate("+ 2*width+", 145)")
+				}else if(extent && d.resid<domain[0]){
+					var translation = ("translate(" + -width + ", 145)");
+				}else{
+					var translation = ("translate(" + (x(d.resid) + x.bandwidth()/2) + ", 145)");
+				}
+				return translation
+			});
+		
+		return this
+	}
+}
+
+class ZHydropathy extends ZChart{
+	constructor(figID, data, snps, seq, snp_tooltips, cutoff_init=0.4) {
+		super(figID, data, snps, seq, snp_tooltips)
+		
+		// Add Y axis			
+		this.svg.append("g")
+			.call(d3.axisLeft(this.y));
+			
+		//Creates the "Mean Hydropathy" y-axis label for Smoothed hydropathy per residue
+		this.svg.append("text")
+			.attr("class", "y label")
+			.attr("text-anchor", "middle")
+			.attr("x", 0 - (this.HEIGHT / 2))
+			.attr("y", this.MARGIN.left - 80)
+			.attr("transform", "rotate(-90)")
+			.text("Mean Hydropathy");
+			
+		this.add_cutoff_line(cutoff_init)
+		this.update_bars(data);
+		
+		return this
+	}
+	
+	update_bars(data, timing=1000, x=this.x, y=this.y) {
+		this.data = data;
+		this.bars.data(data);
+		//console.log(this.bars.data())
+		this.bars.transition()
+			.duration(timing)
+			.attr("y", d => y(d.hydropathy_3_window_mean))
+			.attr("fill", "grey")
+			.attr("height", d => this.HEIGHT - y(d.hydropathy_3_window_mean));
+
+		return this;
+	}
+	
+	add_cutoff_line(my_cut=0.4, x=this.x, y=this.y) {
+		this.cut_line = this.svg.append('g')
+			.append("path")
+			.attr("class", "mypath")
+			.datum(this.data)
+			.attr("fill", "none")
+			.attr("stroke", "steelblue")
+			.attr("stroke-width", 1.5)
+			.attr("d", d3.line()
+				.x((d) => x(d.resid))
+				.y((d) => y(my_cut)));
+
+		return this;
+	}
+	
+	update_cutoff_line(my_cut, x=this.x, y=this.y) {
+		this.cut_line
+			.transition()
+			.duration(1000)
+			.attr("d", d3.line()
+				.x((d) => x(d.resid))
+				.y((d) => y(my_cut)));
+
+		return this;
+	}
+	
+}
+
+
+
+
+
+
 class Figure {
 	constructor(figID, data, snps=0) {
 		// These constants set fixed values for height and width to be used in making all four visualizations
@@ -325,213 +687,8 @@ class Figure {
 		
 		return this
 	}
+
 }
-
-
-
-// Based on this tutorial: https://www.d3-graph-gallery.com/graph/interactivity_zoom.html
-class ZoomableChart extends Figure {
-	constructor(figID, data, snps=0) {
-		// These constants set fixed values for height and width to be used in making all visualizations
-		this.MARGIN = { top: 30, right: 230, bottom: 30, left: 50 };
-		this.WIDTH = 1200 - this.MARGIN.left - this.MARGIN.right;
-		this.HEIGHT = 200 - this.MARGIN.top - this.MARGIN.bottom;
-		
-		this.figID = figID
-		this.data = data
-		
-		let node = document.createElement("div");
-		node.style.position = "relative";
-		this.container = document.getElementById("my_dataviz").appendChild(node);
-		
-		// append the svg object to the body of the page
-		this.Svg = d3.select(this.container)
-		  .append("svg")
-			.attr("width", this.WIDTH + this.MARGIN.left + this.MARGIN.right)
-			.attr("height", this.HEIGHT + this.MARGIN.top + this.MARGIN.bottom)
-		  .append("g")
-			.attr("transform",
-				  "translate(" + this.MARGIN.left + "," + this.MARGIN.top + ")");
-
-		// Add X axis
-		var x = d3.scaleBand()
-			.range([0, this.WIDTH])
-			.domain(data.map(d => d.resid ))
-			.padding(0.2);
-		this.add_xAxis(snps, x)
-/* 		var xAxis = Svg.append("g")
-			.attr("transform", "translate(0," + this.HEIGHT + ")")
-			.call(d3.axisBottom(x)); */
-
-		// Add Y axis
-		this.y = d3.scaleLinear()
-			.domain([0, 1])
-			.range([this.HEIGHT, 0]);
-			
-		this.Svg.append("g")
-			.call(d3.axisLeft(this.y));
-
-		// Add a clipPath: everything out of this area won't be drawn.
-		var clip = this.Svg.append("defs").append("svg:clipPath")
-			.attr("id", "clip")
-			.append("svg:rect")
-			.attr("width", this.WIDTH )
-			.attr("height", this.HEIGHT )
-			.attr("x", 0)
-			.attr("y", 0);
-
-		// Color scale: give me a specie name, I return a color
-/* 		var color = d3.scaleOrdinal()
-			.domain(["setosa", "versicolor", "virginica" ])
-			.range([ "#440154ff", "#21908dff", "#fde725ff"]) */
-
-
-		// Create the plot
-		this.plot = this.Svg.append('g')
-			.attr("clip-path", "url(#clip)");
-	
-
-		this.data = data
-
-		this.bars = this.plot.selectAll("bars")
-			.data(this.data)
-			.join("rect")
-			.attr("id", "barChart"+this.figID)
-			.attr("width", x.bandwidth())
-			.attr("x", (d) => x(d.resid))
-			.attr("y", d => this.HEIGHT)
-			.style("fill", "grey" );
-			
-		this.update_bars(data);
-		
-		// Add brushing
-		var brush = d3.brushX()                 // Add the brush feature using the d3.brush function
-			.extent( [ [0,0], [this.WIDTH, this.HEIGHT] ] ) // initialise the brush area: start at 0,0 and finishes at width,height: it means I select the whole graph area
-			.on("end", updateChart) // Each time the brush selection changes, trigger the 'updateChart' function
-		
-		// Add the brushing
-		this.plot
-			.append("g")
-			  .attr("class", "brush")
-			  .call(brush);
-
-		// A function that set idleTimeOut to null
-		var idleTimeout
-		function idled() { idleTimeout = null; }
-		
-		// A function that update the chart for given boundaries
-		// Make this. variables local variables so they can be used inside updateChart
-		var bars = this.bars
-		var plot = this.plot
-		var y = this.y
-		var width = this.WIDTH
-		var Svg = this.Svg
-		var xAxis = this.xAxis
-		var chart = this
-		const range = ([min, max]) => Array.from({ length: max - min + 1 }, (_, i) => min + i);
-		function updateChart(event) {
-			console.log(this)
-			const extent = event.selection
-			
-			// If no selection, back to initial coordinate. Otherwise, update X axis domain
-			if(!extent){
-			  if (!idleTimeout) return idleTimeout = setTimeout(idled, 350); // This allows to wait a little bit
-			  x.domain(data.map(d => d.resid ))
-			}else{
-			  var domain = [ scaleBandInvert(x)(extent[0]), scaleBandInvert(x)(extent[1]) ]
-			  console.log(range(domain))
-			  x.domain(range(domain))
-			  
-			  plot.select(".brush").call(brush.move, null) // This remove the grey brush area as soon as the selection has been done
-			}
-
-			// Update axis and bar position
-			xAxis.transition().duration(1000).call(d3.axisBottom(x))
-			bars
-			  .transition().duration(1000)
-			  .attr("x", function (d) { 
-				if(extent && d.resid>domain[1]){
-					return width+10
-				}else if(extent && d.resid<domain[0]){
-					return -10;
-				}else{
-					return x(d.resid); 
-				}
-			  })
-			  .attr("y", function (d) { return y(d.hydropathy_3_window_mean); } )
-			  chart.update_xAxis(x)
-		}
-		
-		function scaleBandInvert(scale) {
-		  var domain = scale.domain();
-		  var paddingOuter = scale(domain[0]);
-		  var eachBand = scale.step();
-		  return function (value) {
-			var index = Math.floor(((value - paddingOuter) / eachBand));
-			return domain[Math.max(0,Math.min(index, domain.length-1))];
-		  }
-		}
-	}
-	
-
-	
-	update_bars(data, timing=1000, x=this.x, y=this.y) {
-		this.data = data;
-		this.bars.data(data);
-		//console.log(this.bars.data())
-		this.bars.transition()
-			.duration(timing)
-			.attr("y", d => y(d.hydropathy_3_window_mean))
-			.attr("fill", "grey")
-			.attr("height", d => this.HEIGHT - y(d.hydropathy_3_window_mean));
-
-		return this;
-	}
-	
-	
-	add_xAxis(snps=0, x){
-		if (snps) {
-			var xaxisMargin = this.HEIGHT + 15
-		} else {
-			var xaxisMargin = this.HEIGHT
-		}
-
-		var num_residues = this.data.length
-		
-		this.xAxis = this.Svg.append("g")
-						.attr("transform", "translate(0," + xaxisMargin + ")");
-						
-		this.update_xAxis(x)
-		
-		// Bars
-		//Creates the "Residue" x-axis label
-		if (snps) {
-			var bottomMargin = this.MARGIN.bottom + 25
-		} else {
-			var bottomMargin = this.MARGIN.bottom
-		}
-		this.Svg.append("text")
-			.attr("x", this.WIDTH / 2)
-			.attr("y", this.HEIGHT + bottomMargin)
-			.style("text-anchor", "middle")
-			.text("Residue")
-		
-		return this
-	}
-	
-	update_xAxis(x){
-		console.log(x.domain().length)
-		var tickPeriod = (Math.round((Math.round(x.domain().length/10))/10)*10)
-		this.xAxis.call(d3.axisBottom(x).tickValues(x.domain().filter(function(d, i) { return !((i+1) % 
-						   (tickPeriod) )})));
-		return this
-	}
-}
-
-
-
-
-
 
 class barChart extends Figure {
 	constructor(figID, data, snps, seq, snp_tooltips) {
@@ -731,8 +888,7 @@ class hydropathyPlot extends barChart {
 	}
 
 	add_pathy_ylabel() {
-		//Creates the "Mean Hydropathy" y-axis label for Smoothed hydropathy per residue
-		this.svg.append("text")
+				this.svg.append("text")
 			.attr("class", "y label")
 			.attr("text-anchor", "middle")
 			.attr("x", 0 - (this.GLOBAL_HEIGHT / 2))
@@ -794,7 +950,6 @@ class hydropathyPlot extends barChart {
 	update_bars(data, timing=1000, x=this.x, y=this.y) {
 		this.data = data;
 		this.bars.data(data);
-		//console.log(this.bars.data())
 		this.bars.transition()
 			.duration(timing)
 			.attr("y", d => y(d.hydropathy_3_window_mean))
