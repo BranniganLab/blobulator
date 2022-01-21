67a01b1e6711913653420786b3907230f8c6a18c
Ezry <ezrysm@gmail.com>
2022-01-21 17:19:03 -0500

    All plots now zoomable (independently)

--- static/figure.js
+++ static/figure.js
@@ -168,19 +168,9 @@ class ZChart extends ZFigure{
 			// Update axis and bar position
 			var timing =1000
 			xAxis.transition().duration(timing).call(d3.axisBottom(x))
-			bars
-			  .transition().duration(timing)
-			  .attr("x", function (d) { 
-				if(extent && d.resid>domain[1]){
-					return 2*width
-				}else if(extent && d.resid<domain[0]){
-					return -width;
-				}else{
-					return x(d.resid); 
-				}
-			  })
-			  .attr("y", function (d) { return y(d.hydropathy_3_window_mean); } )
-			  .attr("width", x.bandwidth());
+			//chart.update_bars(data, x, y)
+			chart.zoom_bars(data, x, y, extent, domain)
+
 			chart.update_xAxis(x)
 			if(snps){chart.update_snps(x, extent, domain, width, timing)}
 		}
@@ -309,12 +299,12 @@ class ZHydropathy extends ZChart{
 			.text("Mean Hydropathy");
 			
 		this.add_cutoff_line(cutoff_init)
-		this.update_bars(data);
+		this.update_bars(data, this.x, this.y);
 		
 		return this
 	}
 	
-	update_bars(data, timing=1000, x=this.x, y=this.y) {
+	update_bars(data, x=this.x, y=this.y, timing=1000) {
 		this.data = data;
 		this.bars.data(data);
 		//console.log(this.bars.data())
@@ -351,9 +341,374 @@ class ZHydropathy extends ZChart{
 		return this;
 	}
 	
+	zoom_bars(data, x, y, extent, domain, timing=1000){
+		var width = this.WIDTH
+		this.bars
+		  .transition().duration(timing)
+		  .attr("x", function (d) { 
+			if(extent && d.resid>domain[1]){
+				return 2*width
+			}else if(extent && d.resid<domain[0]){
+				return -width;
+			}else{
+				return x(d.resid); 
+			}
+		  })
+		  .attr("y", function (d) { return y(d.hydropathy_3_window_mean); } )
+		  .attr("width", x.bandwidth());
+	}
+	
 }
 
+class ZblobChart extends ZChart {
+		constructor(figID, data, snps, seq, snp_tooltips, num_residues) {
+		super(figID, data, snps, seq, snp_tooltips, num_residues);
+		this.add_psh_ylabel();
+		this.update_bars(data);
+		this.add_skyline(data, this.x, this.y);
+	}
+	
+	/* add_psh_ylabel
+	FUNCTION: add_psh_ylabel
+	SHORT DESCRIPTION: add an p s h text y label to the svg object
+	INPUTS:
+		svg - a container containing an graph modified by the function itself
+	RETURNS:
+		none
+	*/
+	add_psh_ylabel() {
+		//"p" blob y-axis label for globular tendency plot
+		var ylabel = this.svg.append("g").attr("id", "ylabel")
+		ylabel.append("text")
+			.attr("class", "y label")
+			.attr("text-anchor", "middle")
+			.attr("y", this.HEIGHT-5)
+			.attr("x", this.MARGIN.left-80)
+			.attr("transform", "rotate(0)")
+			.text("p");
 
+		var ylabel = this.svg.append("g").attr("id", "ylabel")
+		ylabel.append("text")
+			.attr("class", "y label")
+			.attr("text-anchor", "middle")
+			.attr("y", this.HEIGHT - 37.5)
+			.attr("x", this.MARGIN.left - 80)
+			.attr("transform", "rotate(0)")
+			.text("s");
+
+
+		//"h" blob y-axis label for globular tendency plot
+		ylabel.append("text")
+			.attr("class", "y label")
+			.attr("text-anchor", "middle")
+			.attr("y", this.HEIGHT - 70)
+			.attr("x", this.MARGIN.left - 80)
+			.attr("transform", "rotate(0)")
+			.text("h");
+
+		//"SNPs" y-axis label for globular tendency plot
+
+		return this;
+	}
+	
+	update_bars(data, timing=1000, x=this.x, y=this.y) {
+		this.data = data;
+		this.bars.data(data);
+		
+		// Lookup table for color attribute of our data as a function of the plot name.
+		// E.g. The "globPlot" plot data stores colors in the "P_diagram" attribute of the data.
+		const figID_to_var = {'ZblobPlot': 'blob_color', 'blobPlot': 'blob_color', 'globPlot': 'P_diagram', 'ncprPlot': 'NCPR_color', 'richPlot': 'h_blob_enrichment',
+			'uverskyPlot': 'uversky_color', 'disorderPlot': 'disorder_color'};
+
+		this.bars.transition()
+			.duration(timing)
+			.attr("y", (d) => y(d.domain_to_numbers))
+			.attr("height", (d) => this.HEIGHT - y(d.domain_to_numbers))
+			.attr("fill", (d) => d[figID_to_var[this.figID]]);
+		
+		// Update/add the corresponding skyline, in a potentially ugly way
+		this.add_skyline(data, x, y);
+		
+		return this;
+	}
+	
+	add_skyline(data, x, y) {
+		// We should have at least two data points to draw a line
+		if(data.length < 2) {
+			return;
+		}
+		
+		// Do we already have a skyline? Remove it
+		if(this.skyline !== undefined) {
+			this.skyline.remove();
+		}
+
+		// Start the line in the correct spot
+		let points = [{resid: data[0].resid, height: data[0].domain_to_numbers}];
+
+		// Find the edges of each "skyscraper"
+		for(let i = 1; i < data.length; i++){
+			let last_res = data[i-1].domain_to_numbers;
+			let this_res = data[i].domain_to_numbers;
+			let resid = data[i].resid;
+			if (last_res != this_res) {
+				points.push({resid: resid, height: last_res});
+				points.push({resid: resid, height: this_res});
+			} 
+		}
+
+		// Last line segment
+		const last_resid = data[data.length-1].resid;
+		points.push({resid: last_resid,
+			height: data[data.length-1].domain_to_numbers});
+
+
+		this.skyline = this.svg.append('g');
+		this.skyline.append("path")
+			.attr("class", "mypath")
+			.datum(points)
+			.attr("fill", "none")
+			.attr("stroke", "grey")
+			.attr("stroke-width", 1.0)
+			.attr("d", d3.line()
+				.x(function (d) {
+					// Extend the final line segment all the way to the right
+					if(d.resid == last_resid) {
+						return x(d.resid) + x.bandwidth();
+					} else {
+						return x(d.resid);
+					}
+				})
+				.y((d) => y(d.height)));
+
+		return this;
+	}
+	
+	/* add_BlobLegend
+	FUNCTION: add_BlobLegend
+	SHORT DESCRIPTION: add the discrete legend for the globular tendencies graph
+	INPUTS:
+		svg - a container containing an graph modified by the function itself
+	RETURNS:
+		none
+	*/
+	add_BlobLegend(keysize=20, offset=20) {
+		//This section contains the key that appears next to the Blob types colored plot
+		//squares for the key
+		var legend = this.svg.append("g").attr("id", "legend")
+
+		legend.append("rect")
+			.attr("x", this.WIDTH + offset)
+			.attr("y", this.MARGIN.top + 5)
+			.attr('width', keysize)
+			.attr('height', keysize)
+			.style("fill", "#0071BC")
+		legend.append("rect")
+			.attr("x", this.WIDTH + offset)
+			.attr("y", this.MARGIN.top + 35)
+			.attr('width', keysize)
+			.attr('height', keysize)
+			.style("fill", "#F7931E")
+		legend.append("rect")
+			.attr("x", this.WIDTH + offset)
+			.attr("y", this.MARGIN.top + 65)
+			.attr('width', keysize)
+			.attr('height', keysize)
+			.style("fill", "#2DB11A")
+				
+		//Text that appears to the right of the key    
+		legend.append("text")
+			.attr("x", this.WIDTH + 50)
+			.attr("y", this.MARGIN.top + 15).text("Hydrophobic blob").style("font-size", "15px")
+			.attr("alignment-baseline", "middle")
+		legend.append("text")
+			.attr("x", this.WIDTH + 50)
+			.attr("y", this.MARGIN.top + 45).text("Hydrophilic blob").style("font-size", "15px")
+			.attr("alignment-baseline", "middle")
+		legend.append("text")
+			.attr("x", this.WIDTH + 50)
+			.attr("y", this.MARGIN.top + 75).text("Short blob").style("font-size", "15px")
+			.attr("alignment-baseline", "middle")
+		return this;
+	}
+	
+	zoom_bars(data, x, y, extent, domain, timing=1000){
+		var width = this.WIDTH
+		this.bars
+		  .transition().duration(timing)
+		  .attr("x", function (d) { 
+			if(extent && d.resid>domain[1]){
+				return 2*width
+			}else if(extent && d.resid<domain[0]){
+				return -width;
+			}else{
+				return x(d.resid); 
+			}
+		  })
+		  .attr("width", x.bandwidth());
+		  
+		this.add_skyline(data, x, y)
+	}
+
+
+	/* add_GlobularLegend
+	FUNCTION: add_GlobularLegend
+	SHORT DESCRIPTION: add the discrete legend for the globular tendencies graph
+	INPUTS:
+		svg - a container containing an graph modified by the function itself
+	RETURNS:
+		none
+	*/
+	add_GlobularLegend(keysize=20, offset=20) {
+		//This section contains the key that appears next to the Globular tendency colored plot
+		//squares for the key
+		var legend = this.svg.append("g").attr("id", "legend")
+
+		legend.append("rect")
+			.attr("x", this.WIDTH + offset)
+			.attr("y", this.MARGIN.top - 25)
+			.attr('width', keysize)
+			.attr('height', keysize)
+			.style("fill", "#0f0")
+		legend.append("rect")
+			.attr("x", this.WIDTH + offset)
+			.attr("y", this.MARGIN.top + 5)
+			.attr('width', keysize)
+			.attr('height', keysize)
+			.style("fill", "#FEE882")
+		legend.append("rect")
+			.attr("x", this.WIDTH + offset)
+			.attr("y", this.MARGIN.top + 35)
+			.attr('width', keysize)
+			.attr('height', keysize)
+			.style("fill", "#BF72D2")
+		legend.append("rect")
+			.attr("x", this.WIDTH + offset)
+			.attr("y", this.MARGIN.top + 65)
+			.attr('width', keysize)
+			.attr('height', keysize)
+			.style("fill", "#f00")
+		legend.append("rect")
+			.attr("x", this.WIDTH + offset)
+			.attr("y", this.MARGIN.top + 95)
+			.attr('width', keysize)
+			.attr('height', keysize)
+			.style("fill", "#00f")
+				
+		//Text that appears to the right of the key    
+		legend.append("text")
+			.attr("x", this.WIDTH + 50)
+			.attr("y", this.MARGIN.top - 15).text("Globular").style("font-size", "15px")
+			.attr("alignment-baseline", "middle")
+		legend.append("text")
+			.attr("x", this.WIDTH + 50)
+			.attr("y", this.MARGIN.top + 15).text("Janus/Boundary").style("font-size", "15px")
+			.attr("alignment-baseline", "middle")
+		legend.append("text")
+			.attr("x", this.WIDTH + 50)
+			.attr("y", this.MARGIN.top + 45).text("Strong Polyelectrolyte").style("font-size", "15px")
+			.attr("alignment-baseline", "middle")
+		legend.append("text")
+			.attr("x", this.WIDTH + 50)
+			.attr("y", this.MARGIN.top + 75).text("Strong Polyanion (-)").style("font-size", "15px")
+			.attr("alignment-baseline", "middle")
+		legend.append("text")
+			.attr("x", this.WIDTH + 50)
+			.attr("y", this.MARGIN.top + 105).text("Strong Polycation (+)").style("font-size", "15px")
+			.attr("alignment-baseline", "middle")
+
+		return this;
+	}
+
+	add_ContinuousLegend(cmap, {min='-1', med='0', max='+1', width=20, height=80}={min: '-1', med: '0', max: '+1', width: 20, height: 80}) {
+		switch(cmap) {
+			case "PuOr":
+				this.add_colorbar("PuOr", width, height, min, max, this.WIDTH, this.HEIGHT,
+					{ med: med, cend: '#7f3b08', cq3: '#ee9d3c', cmid: '#f6f6f7', ctop: '#2d004b' });
+				break;
+			case "RWB":
+				//Color bar key to the right of the enrichment plot.
+				this.add_colorbar("RWB", width, height, min, max, this.WIDTH, this.HEIGHT,
+					{ med: med, cend: '#ff0000', cmid: '#f4f4ff', ctop: '#0000ff' });
+				break;
+			default:
+				try {
+					throw cmap
+				} catch(e) {
+					console.log("Figure.add_ContinuousLegend doesn't know colormap: "+e)
+				}
+		}
+
+		return this;
+	}
+	
+	/* add_colorbar
+    Function to create linear color bars for graphs. Adapted from: https://www.visualcinnamon.com/2016/05/smooth-color-legend-d3-svg-gradient/
+    Inputs: 
+      svg container,
+      width and height of the colorbar in px
+      min and max values
+      x and y position of the upper left corner
+      ctop and cend for the first and last colors of the gradient
+      Unique id string
+      optional arguments: cq1, cmid, cq3 which define the colors at 25%, 50%, and 75% respectively.d
+    Returns: 
+        none - svg is passed "by reference"
+	*/
+	add_colorbar(id, width, height, min, max, xpos, ypos, {ctop, cend, med, cq1, cmid, cq3}={}) {
+		this.add_linearGradient(id, ctop, cend, {cq1, cmid, cq3})
+
+		//Draw the rectangle and fill with the gradient
+		this.svg.append("rect").attr("width", width).attr("height", height).attr("y", ypos-height).attr("x", xpos+width).style("fill", "url(#"+id+")");
+
+		//Add labels
+		this.svg.append("text").attr("x", xpos+2*width+6).attr("y", ypos-height+6).text(max)
+		
+		if (med){this.svg.append("text").attr("x", xpos+2*width+6).attr("y", (2*ypos-height+12)/2).text(med)}
+		
+		this.svg.append("text").attr("x", xpos+2*width+6).attr("y", ypos+6).text(min)
+
+		return this
+	}
+	
+		/* add_linearGradient:
+        Function to create linear color bars for graphs. Adapted from: https://www.visualcinnamon.com/2016/05/smooth-color-legend-d3-svg-gradient/
+		Inputs: 
+			svg container, 
+			ctop and cend for the first and last colors of the gradient
+			an id string
+			optional arguments: cq1, cmid, cq3 which define the colors at 25%, 50%, and 75% respectively.d
+		Returns:
+			The svg with a linear gradient with the given id
+	*/
+	add_linearGradient(id, ctop, cend, {cq1, cmid, cq3}={}) {
+		var defs = this.svg.append("defs");
+
+		//Append a linearGradient element to the defs and give it a unique id
+		var linearGradient = defs.append("linearGradient").attr("id", id).attr("x1", "0%").attr("y1", "0%").attr("x2", "0%").attr("y2", "100%");
+
+		//Define the gradient
+		//Set the color for the start (0%)
+		linearGradient.append("stop").attr("offset", "0%").attr("stop-color", ctop);
+		//Set the color for the end (25%)
+		if (cq1) {
+			linearGradient.append("stop").attr("offset", "25%").attr("stop-color", cq1);
+		}
+		//Set the color for the end (50%)
+		if (cmid) {
+			linearGradient.append("stop").attr("offset", "50%").attr("stop-color", cmid);
+		}
+		//Set the color for the end (75%)
+		if (cq3) {
+			linearGradient.append("stop").attr("offset", "75%").attr("stop-color", cq3);
+		}
+		//Set the color for the end (100%)
+		linearGradient.append("stop").attr("offset", "100%").attr("stop-color", cend);
+
+		return this;
+	}
+
+}
 
 
 
@@ -875,7 +1230,7 @@ class blobChart extends barChart {
 	}
 }
 
-class hydropathyPlot extends barChart {
+/* class hydropathyPlot extends barChart {
 	constructor(figID, data, snps, seq, snp_tooltips, cutoff_init=0.4) {
 		super(figID, data, snps, seq, snp_tooltips);
 		this.add_cutoff_line(cutoff_init);
@@ -931,7 +1286,7 @@ class hydropathyPlot extends barChart {
 
 	/* add_hydropathy_bars
 	*/
-	add_hydropathy_bars(x=this.x, y=this.y) {
+/* 	add_hydropathy_bars(x=this.x, y=this.y) {
 		this.bars = this.svg.selectAll("mybar")
 		this.bars.data(this.data)
 			.enter()
@@ -956,4 +1311,4 @@ class hydropathyPlot extends barChart {
 
 		return this;
 	}
-}
+} */ 
