099e277ccf7865a369b4e7e8de092cf2a1ea5678
EzryStIago <ezrysm@gmail.com>
2023-01-11 13:53:24 -0500

    testing hydropathy slider

--- regression_tests/tests/test_search.py
+++ regression_tests/tests/test_search.py
@@ -37,6 +37,7 @@ def test_uniprot_search(
     expect(result_page.uniprot_id).to_contain_text(uniprot_id)
 
 
+# When the mutation button is checked with a SNP
 def test_snp_button(page: Page, uniprot_results: BlobulatorResultPage):
     # given the results page is loaded
     result_page = uniprot_results
@@ -57,3 +58,127 @@ def test_snp_button(page: Page, uniprot_results: BlobulatorResultPage):
 
     # And the snp triangle to be red
     expect(result_page.snps.nth(snp_id)).to_have_attribute("fill", "red")
+
+
+# After adjusting the hydropathy slider
+def test_hydropathy_slider(page: Page, uniprot_results: BlobulatorResultPage):
+    # given the results page is loaded
+    result_page = uniprot_results
+
+    # Get initial conditions
+    cutoff_indicator_y = result_page.hydropathy_line.get_attribute("y1")
+    print(cutoff_indicator_y)
+
+    # When the hydropathy slider is set to 0.2
+    result_page.hydropathy_slider.hover()
+    result_page.mouse.down()
+
+    target_hydropathy = 0.2
+    hydropathy = 0.4
+    current_x = 10000
+    prev_x = 0
+    while hydropathy != target_hydropathy:
+        step = abs(current_x - prev_x) / 2
+
+        prev_x = current_x
+
+        if hydropathy < target_hydropathy:
+            current_x += step
+        else:
+            current_x -= step
+
+        result_page.mouse.move(current_x, 0)
+        hydropathy = float(result_page.hydropathy_field.input_value())
+
+    result_page.mouse.up()
+
+    # Expect the cutoff indicator has moved down
+
+    # Expect the numerical input is updated to 0.2
+    expect(result_page.hydropathy_field).to_have_value(str(target_hydropathy))
+    # Expect the blob chart to change heights
+    # Expect the blob chart to change colors
+
+
+# Given the results page is loaded with P37840
+# When the hydropathy slider is set to 0.6
+# Expect the numerical input is updated to 0.2
+# Expect the cutoff indicator has moved up
+# Expect the blob chart to change heights
+# Expect the blob chart to change colors
+
+
+# After adjusting the blob size slider
+# Given the results page is loaded with P37840
+# When the blob size slider is set to 10
+# Expect the numerical input is updated to 10
+# Expect the blob chart to change heights
+# Expect the blob chart to change colors
+
+# Given the results page is loaded with P37840
+# When the blob size slider is set to 1
+# Expect the numerical input is updated to 1
+# Expect the blob chart to change heights
+# Expect the blob chart to change colors
+
+
+# After adjusting the hydropathy numerical input
+# Given the results page is loaded with P37840
+# When the hydropathy numerical input is set to 1
+# Expect the hydropathy slider moves left
+# Expect the blob chart to change heights
+# Expect the blob chart to change colors
+
+# Given the results page is loaded with P37840
+# When the hydropathy numerical input is set to 10
+# Expect the hydropathy slider moves right
+# Expect the blob chart to change heights
+# Expect the blob chart to change colors
+
+
+# After adjusting the blob size numerical input
+
+
+# After clicking the hydropathy up and down buttons
+
+# After clicking the blob size up and down buttons
+
+# Check the download plots button
+
+# Check the download data button
+
+# Check that website links lead to the expected papers and websites
+
+# Check multiple SNP residues and make sure that they update correctly after selecting a new SNP
+
+# Run current regression tests with an introduced mutation.
+
+# Check uniprot and ensemble IDs for both a legitimate and false ID. Make sure that the expected protein is blobulated, and a valid error is shown in the case of a false ID
+
+# When the results page is loaded (on a non-full screen browser window) are all plots and “i” icons visible?
+
+# For each track, are all bars visible?
+
+# Load a large protein, does the browser load within 1 minute?
+
+# Check links for SNP triangles, make sure they lead to the expected website
+
+# Does everything on both the result and home pages look aligned?
+
+# When clickable elements are selected, do they look as expected?
+
+# Does the terminal output anything when blobulation occurs?
+
+# Are SNP triangles centered over the correct residue?
+
+# Does the skyline update around the correct residues?
+
+# Does the result controls panel stay at the top of the page when scrolling?
+
+# When zooming, check that all figures update, resid labels are legible, and the skyline updates correctly.
+
+# Is the control panel correctly sized?
+
+# Do all tabs contain their expected content?
+
+# Blobulate the sequence GSGPPGPPGPPGPPGARGQAGVMGFPGPPGPPGPPGRAPTDQHIKQVCMRVIQEHFAEMAASLKRPDSGAT, and check that the smoothed kyte-doolittle hydropathies match those given by https://www.ebi.ac.uk/Tools/seqstats/emboss_pepinfo/
