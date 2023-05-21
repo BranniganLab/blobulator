92af9337fd724835cbfdd587cfec854c17c0c2e2
Ezry <ezrysm@gmail.com>
2023-05-21 14:40:17 -0400

    Cleanup unneeded imports

--- examples/blobulate_sequence.py
+++ examples/blobulate_sequence.py
@@ -1,27 +1,22 @@
 """
 A Hello-World for blobulation in a python script. Blobulate 
 """
-if __name__ == "__main__":
 
-    import argparse
-    from Bio import SeqIO
-    from Bio.Seq import Seq
+import blobulator
 
-    import blobulator
+# A very simple oligopeptide
+sequence = "RRRRRRRRRIIIIIIIII"
 
-    # A very simple oligopeptide
-    sequence = "RRRRRRRRRIIIIIIIII"
+# Default cutoff, Lmin, and hydrophobicity scale
+cutoff = 0.4
+min_blob = 4
+hscale = "kyte_doolittle"
 
-    # Default cutoff, Lmin, and hydrophobicity scale
-    cutoff = 0.4
-    min_blob = 4
-    hscale = "kyte_doolittle"
+# Output name
+oname = "hello_blob.csv"
 
-    # Output name
-    oname = "hello_blob.csv"
-
-    print(f'Running...\nseq: {sequence}\ncutoff: {cutoff}\nminBlob: {min_blob}\nOutput to: {oname}')
-    blobDF = blobulator.compute(sequence, cutoff, min_blob, hscale)
-    print ("Writing output file")
-    blobDF = blobulator.clean_df(blobDF)
-    blobDF.to_csv(oname, index=False)
+print(f'Running...\nseq: {sequence}\ncutoff: {cutoff}\nminBlob: {min_blob}\nOutput to: {oname}')
+blobDF = blobulator.compute(sequence, cutoff, min_blob, hscale)
+print ("Writing output file")
+blobDF = blobulator.clean_df(blobDF)
+blobDF.to_csv(oname, index=False)
