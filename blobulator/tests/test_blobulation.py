5ea3c38cdb1a4383229591f22f3efceb227708d2
EzryStIago <ezrysm@gmail.com>
2023-12-05 18:10:33 -0500

    add documentation and encoding specs

--- blobulator/tests/test_blobulation.py
+++ blobulator/tests/test_blobulation.py
@@ -12,12 +12,27 @@ with open("expected_inputs.txt", 'r') as ifile:
 class TestBlobulation:
     @pytest.mark.parametrize("sequence, cutoff, min_blob, oname", test_data)
     def test_sequence_outputs(self, sequence:str, cutoff:float, min_blob:int, oname:str, hscale='kyte_doolittle'):
+        """
+        Perform regression testing by comparing new outputs with canonical outputs.
+
+        Args:
+            self: The instance of the TestBlobulation class.
+            sequence (str): The DNA sequence to be processed.
+            cutoff (float): The cutoff value for blobulation.
+            min_blob (int): The minimum blob size.
+            oname (str): The name of the output file.
+            hscale (str, optional): The hscale parameter for blobulation. Defaults to 'kyte_doolittle'.
+
+        Returns:
+            None. The method performs operations on the input parameters and asserts the equality of the generated CSV file with the expected output file.
+        """
 
         blobDF = blobulator.compute(sequence, float(cutoff), int(min_blob), hscale)
         blobDF = blobulator.clean_df(blobDF)
-        blobDF.to_csv(f"test_outputs/{oname[0:-13]}.csv", index=False)
+        blobDF.to_csv(f"test_outputs/{oname[0:-13]}_test.csv", index=False, encoding='utf-8')
 
-        expected = [row for row in open(oname)]
-        test = [row for row in open(f"test_outputs/{oname[0:-13]}.csv")]
+        expected = list(open(f"canonical_outputs/{oname}", encoding='utf-8'))
+        test = list(open(f"test_outputs/{oname[0:-13]}_test.csv", encoding='utf-8'))
 
-        assert expected == test
+        for i, j in zip(expected, test):
+            assert i == j
