46f62124baa9b8664451db99f2e9a5131b5fe671
Ryan Lamb <rwl54@sirona.ccib.rutgers.edu>
2024-06-27 12:25:55 -0400

    Increased spectrum of color scale to make blob colors less similar.

--- VMD_scripts/Blob_GUI.tcl
+++ VMD_scripts/Blob_GUI.tcl
@@ -336,10 +336,11 @@ proc tricolor_scale {PointA PointB ColorA ColorB} {
 }
 
 
-proc colorScale {{pointA 0} {pointB 256} {pointC 512} {pointD 768} {pointE 1024} {colorA "0.0 0.5 0.5"} {colorB "0.4 1.0 1.0"} {colorC "0.0 0.3 0.6"} {colorD "0.4 0.7 1.0"} {colorE "0.0 0.0 0.6"}} {
+proc colorScale {{pointA 0} {pointB 205} {pointC 410} {pointD 615} {pointE 820} {pointF 1024} {colorA "0.0 0.5 0.5"} {colorB "0.4 1.0 1.0"} {colorC "0.0 0.3 0.6"} {colorD "0.4 0.7 1.0"} {colorE "0.0 0.0 0.6"} {colorF "0.2 0.2 1.0"}} {
 	tricolor_scale $pointA $pointB $colorA $colorB
 	tricolor_scale $pointB $pointC $colorB $colorC
 	tricolor_scale $pointC $pointD $colorC $colorD
 	tricolor_scale $pointD $pointE $colorD $colorE
+	tricolor_scale $pointE $pointF $colorE $colorF
 }
 
