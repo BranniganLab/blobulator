acb4e905ff26f23071d3853e736dee519e47943f
cpitman-11 <pitman.connor@rutgers.edu>
2024-07-25 10:29:51 -0400

    molstar now accepts a pdb file as a string instead of a url, NOTE: still need to read in user-inputted pdb file as a string

--- website/static/molstar_plugin/plugin/src/index.ts
+++ website/static/molstar_plugin/plugin/src/index.ts
@@ -1,23 +1,16 @@
-
 import { DefaultPluginUISpec, PluginUISpec } from '../../node_modules/molstar/lib/mol-plugin-ui/spec';
 import { createPluginUI } from '../../node_modules/molstar/lib/mol-plugin-ui';
 import { renderReact18 } from '../../node_modules/molstar/lib/mol-plugin-ui/react18';
 import { PluginConfig } from '../../node_modules/molstar/lib/mol-plugin/config';
-import { PluginContext } from '../../node_modules/molstar/lib/mol-plugin/context'
+import { parsePDB } from '../../node_modules/molstar/lib/mol-io/reader/pdb/parser';
+import { Viewer } from '../../node_modules/molstar/lib/apps/viewer';
 
 import { MolScriptBuilder as MS, MolScriptBuilder } from '../../node_modules/molstar/lib/mol-script/language/builder';
-import { Expression } from '../../node_modules/molstar/lib/mol-script/language/expression';
-import {  StructureSelectionQuery } from '../../node_modules/molstar/lib/mol-plugin-state/helpers/structure-selection-query';
-import {
-  Structure,
-  StructureProperties,
-} from "../../node_modules/molstar/lib/mol-model/structure";
-import { Script } from '../../node_modules/molstar/lib/mol-script/script';
 import { Color } from '../../node_modules/molstar/lib/mol-util/color';
-import { ColorNames } from '../../node_modules/molstar/lib/mol-util/color/names'
 
 import { StateTransforms } from '../../node_modules/molstar/lib/mol-plugin-state/transforms';
 import { createStructureRepresentationParams } from '../../node_modules/molstar/lib/mol-plugin-state/helpers/structure-representation-params'
+
 
 const MySpec: PluginUISpec = {
     ...DefaultPluginUISpec(),
@@ -61,10 +54,29 @@ async function createPlugin(parent: HTMLElement) {
                 ]
             }
     });
+    const fs = require('fs')
+    const content = fs.readFile('../../../pdb_files/current.pdb', 'utf8')
 
 
-    const data = await plugin.builders.data.download({ url: "https://files.rcsb.org/download/1xq8.pdb" }, { state: { isGhost: true } });
+    // A misc mix of things that I've tried, currently the const data = ... is working as expected, now need to get contents of the input file to be read in as a string
+    // const data = await plugin.builders.data.download({ url: 'https://raw.githubusercontent.com/vis4moleculer/molstar/master/tests/data/1crn.cif' }, { state: { isGhost: true } });
+    // const data = await Viewer.loadStructureFromUrl('../../../pdb_files/current.pdb')
+    // const file = document.getElementById('pdb_file');
+    // const fileContent = await file.text();
+    // const fileURL  = URL.createObjectURL(file)
+    // const reader = new FileReader()
+    // const fileURL = reader.readAsDataURL(file)
+    // const fileContents = readFileSync(file).toString();
+    // var pdbFile = parsePDB('../../../pdb_files/current.pdb', 'pdb', true);
+    // const readFile = plugin.builders.data.readFile({ file: pdbFile });
+    // const data = await Viewer.loadStructureFromUrl('../../../pdb_files/current.pdb', format='pdb')
+    // const data = await plugin.builders.data.download({ url: path.dirname('../../../../pdb_files/current.pdb') }, { state: { isGhost: true } });
+    // const data = await plugin.builders.data.rawData({ data: readFile}, { state: { isGhost: true } });
+
+    // This version of data works when the entire pdb is submitted as a string
+    const data = await plugin.builders.data.rawData({data: content})
     const trajectory = await plugin.builders.structure.parseTrajectory(data, 'pdb');
+    
     // await plugin.builders.structure.hierarchy.applyPreset(trajectory, 'default');
 
     const model = await plugin.builders.structure.createModel(trajectory);
@@ -80,7 +92,7 @@ async function createPlugin(parent: HTMLElement) {
     builder.buildRepresentation(update, components.polymer, { type: 'cartoon', typeParams: { alpha: 0.0 }, color : 'uniform', colorParams: { value: Color(0xFFA500) } }, { tag: 'polymer' });
     // await update.commit();
 
-    let p_arr = [10, 11, 12, 13,  20, 21, 22, 23, 24, , 32, 33, 34, 35, 36, 42, 43, 44, 45, 46, 57, 58, 59, 60, 61, 62, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140]
+    let p_arr = [10, 11, 12, 13,  20, 21, 22, 23, 24,  32, 33, 34, 35, 36, 42, 43, 44, 45, 46, 57, 58, 59, 60, 61, 62, 93, 94, 95, 96, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140]
     let h_arr = [[1, 2, 3, 4, 5, 6, 7, 8, 9], [14, 15, 16, 17, 18, 19], [25, 26, 27, 28, 29, 30, 31], [37, 38, 39, 40, 41], [47, 48, 49, 50, 51, 52, 53, 54, 55, 56], [63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78], [81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92, 93, 94, 95, 96]]
     let s_arr = [79, 80]
 
