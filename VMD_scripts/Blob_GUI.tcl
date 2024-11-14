93ec2324f9735a40c7a953363504f77ee8be6179
Ryan Lamb <rwl54@sirona.ccib.rutgers.edu>
2024-11-14 13:59:22 -0500

    added comments to variables the set initial values.

--- VMD_scripts/Blob_GUI.tcl
+++ VMD_scripts/Blob_GUI.tcl
@@ -36,8 +36,11 @@ namespace eval ::blobulator {
 
 
 	#Switch variables and sets intital values
+	#Sets the initial hydrophobicity scale
 	variable hydropathyScaleDictionaryList "Kyte-Doolittle"
+	#Sets the initial atomselection
 	variable select "all"
+	
 	variable resStart 
 	variable resEnd
 	variable Lmin 4
