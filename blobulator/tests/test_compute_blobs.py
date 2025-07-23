3f766cc696680de9d08ef62afa4fa309b542f79a
cpitman-11 <pitman.connor@rutgers.edu>
2025-07-23 18:22:37 -0400

    adding test to ensure that outputs of future versions of compute_blobs.py are consistent with the current

--- blobulator/tests/test_compute_blobs.py
+++ blobulator/tests/test_compute_blobs.py
@@ -1,5 +1,30 @@
 import pytest
+import blobulator
+import os
+import pandas as pd
+
 
 def test_CI():
     """ Simple test to make sure CI is working"""
     assert True
+
+def test_blobulator():
+    """ A test to make sure that the current compute_blobs.py has the same output as the old compute_blobs.py  """
+
+    A very simple oligopeptide and standard settings
+    sequence = "RRRRRRRRRIIIIIIIII"
+    cutoff = 0.4
+    min_blob = 4
+    hscale = "kyte_doolittle"
+
+    # Do the blobulation
+    blobDF = blobulator.compute(sequence, cutoff, min_blob, hscale)
+    
+    # Cleanup the dataframe (make it more human-readable)
+    blobDF = blobulator.clean_df(blobDF)
+    
+    # Save it as a csv for later use
+    testDF = pd.read_csv("blobulator/tests/test_blob.csv")
+
+    assert blobDF == testDF
+    
