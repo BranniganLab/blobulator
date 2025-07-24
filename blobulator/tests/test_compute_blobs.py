85173d5dfccb5ac7bd223a1e55682763a1261a64
cpitman-11 <csp127@scarletmail.rutgers.edu>
2025-07-24 17:03:33 -0400

    adding pytest fixtures to define datasets, comparing columns of the dfs not the whole df at once

--- blobulator/tests/test_compute_blobs.py
+++ blobulator/tests/test_compute_blobs.py
@@ -3,34 +3,53 @@ import blobulator
 import pandas as pd
 import os
 
+@pytest.fixture
+def current_blobulation():
+    sequence = "RRRRRRRRRIIIIIIIII"
+    cutoff = 0.4
+    min_blob = 4
+    hscale = "kyte_doolittle"
+    
+    bltestBlobDFobDF = blobulator.compute(sequence, cutoff, min_blob, hscale)
+    
+    testBlobDF = blobulator.clean_df(blobDF)
+    
+    return testBlobDF
+@pytest.fixture
+def previous_blobulator_output():
+    previousBlobDF = pd.read_csv("blobulator/tests/test_blob.csv")
+    return previousBlobDF
+
 
 def test_CI():
     """ Simple test to make sure CI is working"""
     assert True
 
-def test_blobulator():
-    """ A test to make sure that the current compute_blobs.py has the same output as the old compute_blobs.py  """
+def test_min_blob_hydropathy(testBlobDF, previousBlobDF):
+    """ Tests that the hydropathy column is consistent between both the old and new blobulator outputs"""
+    assert testBlobDF["Min_Blob_Hydropathy"] == previousBlobDF["Min_Blob_Hydropathy"]
 
-    # A very simple oligopeptide and standard settings
-    sequence = "RRRRRRRRRIIIIIIIII"
-    cutoff = 0.4
-    min_blob = 4
-    hscale = "kyte_doolittle"
+def test_blob_index_number(testBlobDF, previousBlobDF):
+    """ Tests that the index column is consistent between both the old and new blobulator outputs"""
+    assert testBlobDF["Blob_Index_Number"] == previousBlobDF["Blob_Index_Number"]
 
-    # Do the blobulation
-    blobDF = blobulator.compute(sequence, cutoff, min_blob, hscale)
-    
-    # Cleanup the dataframe (make it more human-readable)
-    blobDF = blobulator.clean_df(blobDF)
+def test_blob_ncpr(testBlobDF, previousBlobDF):
+    """ Tests that the NCPR column is consistent between both the old and new blobulator outputs"""
+    assert testBlobDF["Blob_NCPR"] == previousBlobDF["Blob_NCPR"]
 
-    oname = "hello_blob.csv"
-    blobDF.to_csv(oname, index=False)
-    
-    # Save it as a csv for later use
-    newDF = pd.read_csv("hello_blob.csv")
-    testDF = pd.read_csv("blobulator/tests/test_blob.csv")
+def test_blob_dsnp_enrichment(testBlobDF, previousBlobDF):
+    """ Tests that the dSNP_enrichment column is consistent between both the old and new blobulator outputs"""
+    assert testBlobDF["dSNP_enrichment"] == previousBlobDF["dSNP_enrichment"]
 
-    os.remove("hello_blob.csv")
+def test_blob_daspappu(testBlobDF, previousBlobDF):
+    """ Tests that the Das-Pappu class column is consistent between both the old and new blobulator outputs"""
+    assert testBlobDF["Blob_Das-Pappu_Class"] == previousBlobDF["Blob_Das-Pappu_Class"]
 
-    assert blobDF == testDF
-    
+def test_blob_uversky(testBlobDF, previousBlobDF):
+    """ Tests that the uversky column is consistent between both the old and new blobulator outputs"""
+    assert testBlobDF["Uversky_Diagram_Score"] == previousBlobDF["Uversky_Diagram_Score"]
+
+def test_smoothed_hydropathy(testBlobDF, previousBlobDF):
+    """ Tests that the smoothed hydropathy column is consistent between both the old and new blobulator outputs"""
+    assert testBlobDF["Smoothed_Hydropathy"] == previousBlobDF["Smoothed_Hydropathy"]
+
