43083bd67e117e9071174a59650d780ab3826e3a
cpitman-11 <csp127@scarletmail.rutgers.edu>
2025-07-24 17:36:01 -0400

    trying assert_series_equal

--- blobulator/tests/test_compute_blobs.py
+++ blobulator/tests/test_compute_blobs.py
@@ -1,6 +1,7 @@
 import pytest
 import blobulator
 import pandas as pd
+from pandas import testing as tm
 import os
 
 @pytest.fixture
@@ -27,29 +28,29 @@ def test_CI():
 
 def test_min_blob_hydropathy(current_blobulation, previous_blobulator_output):
     """ Tests that the hydropathy column is consistent between both the old and new blobulator outputs"""
-    assert current_blobulation["Min_Blob_Hydropathy"].equals(previous_blobulator_output["Min_Blob_Hydropathy"])
+    assert tm.assert_series_equal(current_blobulation["Min_Blob_Hydropathy"], previous_blobulator_output["Min_Blob_Hydropathy"])
 
 def test_blob_index_number(current_blobulation, previous_blobulator_output):
     """ Tests that the index column is consistent between both the old and new blobulator outputs"""
-    assert current_blobulation["Blob_Index_Number"].equals(previous_blobulator_output["Blob_Index_Number"])
+    assert tm.assert_series_equal(current_blobulation["Blob_Index_Number"], previous_blobulator_output["Blob_Index_Number"])
 
 def test_blob_ncpr(current_blobulation, previous_blobulator_output):
     """ Tests that the NCPR column is consistent between both the old and new blobulator outputs"""
-    assert current_blobulation["Blob_NCPR"].equals(previous_blobulator_output["Blob_NCPR"])
+    assert current_blobulation["Blob_NCPR"], previous_blobulator_output["Blob_NCPR"])
 
 def test_blob_dsnp_enrichment(current_blobulation, previous_blobulator_output):
     """ Tests that the dSNP_enrichment column is consistent between both the old and new blobulator outputs"""
-    assert current_blobulation["dSNP_enrichment"].equals(previous_blobulator_output["dSNP_enrichment"])
+    assert tm.assert_series_equal(current_blobulation["dSNP_enrichment"], previous_blobulator_output["dSNP_enrichment"])
 
 def test_blob_daspappu(current_blobulation, previous_blobulator_output):
     """ Tests that the Das-Pappu class column is consistent between both the old and new blobulator outputs"""
-    assert current_blobulation["Blob_Das-Pappu_Class"].equals(previous_blobulator_output["Blob_Das-Pappu_Class"])
+    assert tm.assert_series_equal(current_blobulation["Blob_Das-Pappu_Class"], previous_blobulator_output["Blob_Das-Pappu_Class"])
 
 def test_blob_uversky(current_blobulation, previous_blobulator_output):
     """ Tests that the uversky column is consistent between both the old and new blobulator outputs"""
-    assert current_blobulation["Uversky_Diagram_Score"].equals(previous_blobulator_output["Uversky_Diagram_Score"])
+    assert tm.assert_series_equal(current_blobulation["Uversky_Diagram_Score"], previous_blobulator_output["Uversky_Diagram_Score"])
 
-def test_smoothed_hydropathy(current_blobulation, previous_blobulator_output):
-    """ Tests that the smoothed hydropathy column is consistent between both the old and new blobulator outputs"""
-    assert current_blobulation["Smoothed_Hydropathy"].equals(previous_blobulator_output["Smoothed_Hydropathy"])
+def test_kytedoolittle_hydropathy(current_blobulation, previous_blobulator_output):
+    """ Tests that the Kyte-Doolittle hydropathy column is consistent between both the old and new blobulator outputs"""
+    assert tm.assert_series_equal(current_blobulation["Kyte-Doolittle_hydropathy"], previous_blobulator_output["Kyte-Doolittle_hydropathy"])
 
