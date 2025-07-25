f763ca9a8a6e89578b553fe6581f6ab5c818a434
EzryStIago <ezrysm@gmail.com>
2025-07-25 11:30:06 -0400

    test:try with approvaltests.

--- blobulator/tests/test_compute_blobs.py
+++ blobulator/tests/test_compute_blobs.py
@@ -1,6 +1,10 @@
 import pytest
 import blobulator
 import pandas as pd
+from approvaltests import verify
+
+pd.set_option('display.max_rows', None)
+pd.set_option('display.max_columns', None)
 
 @pytest.fixture
 def current_blobulation():
@@ -20,13 +24,12 @@ def previous_blobulator_output():
     return previous_blobulator_output
 
 
-def test_CI():
-    """ Simple test to make sure CI is working"""
-    assert True
+def test_verify_whole_df(current_blobulation):
+    verify(current_blobulation)
 
-def test_min_blob_hydropathy(current_blobulation, previous_blobulator_output):
+def test_min_blob_hydropathy(current_blobulation):
     """ Tests that the hydropathy column is consistent between both the old and new blobulator outputs"""
-    assert current_blobulation["Min_Blob_Hydropathy"].round(3).astype(str).equals(previous_blobulator_output["Min_Blob_Hydropathy"].round(3).astype(str))
+    verify(current_blobulation["Min_Blob_Hydropathy"].round(3).astype(str))
 
 def test_blob_index_number(current_blobulation, previous_blobulator_output):
     """ Tests that the index column is consistent between both the old and new blobulator outputs"""
