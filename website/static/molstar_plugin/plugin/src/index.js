179ecb08f81434b8870af6a76e6d3246a80e0f78
cpitman-11 <pitman.connor@rutgers.edu>
2025-07-28 16:17:57 -0400

    removing new functions to see if drag and drop functionality not working persists

--- website/static/molstar_plugin/plugin/src/index.js
+++ website/static/molstar_plugin/plugin/src/index.js
@@ -178,7 +178,7 @@ function createBlobRepresentation(plugin) {
 ;
 function createPlugin(parent) {
     return __awaiter(this, void 0, void 0, function () {
-        var defaultSpec, plugin, elementsArray, dropwindow, molstarWindow;
+        var defaultSpec, plugin, elementsArray;
         return __generator(this, function (_a) {
             switch (_a.label) {
                 case 0:
@@ -216,27 +216,6 @@ function createPlugin(parent) {
                                 createBlobRepresentation(plugin);
                             }, 1000);
                         });
-                    });
-                    dropwindow = document.querySelector('#app');
-                    dropwindow === null || dropwindow === void 0 ? void 0 : dropwindow.addEventListener('drop', function (event) {
-                        event.preventDefault();
-                        setTimeout(function () {
-                            createBlobRepresentation(plugin);
-                        }, 1000);
-                    });
-                    molstarWindow = document.querySelector('#app');
-                    molstarWindow === null || molstarWindow === void 0 ? void 0 : molstarWindow.addEventListener('drop', function (event) {
-                        var _a;
-                        event.preventDefault();
-                        var file = (_a = event.dataTransfer) === null || _a === void 0 ? void 0 : _a.files[0];
-                        var reader = new FileReader();
-                        reader.onload = function () {
-                            localStorage.setItem("pdb_file", reader.result);
-                            createBlobRepresentation(plugin);
-                            var molstar_warning_box = document.getElementById("molstar_warning_box");
-                            molstar_warning_box.innerHTML = "";
-                        };
-                        reader.readAsText(file);
                     });
                     return [2 /*return*/];
             }
