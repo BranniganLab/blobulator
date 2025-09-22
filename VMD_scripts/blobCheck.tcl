74ff8479c9beaf40af940019d9d2ceff4c346846
Ryan Lamb <rwl54@sirona.ccib.rutgers.edu>
2025-09-22 12:58:53 -0400

    Fixed file being set the name of the variable instead of the string literal the variable represents.

--- VMD_scripts/blobCheck.tcl
+++ VMD_scripts/blobCheck.tcl
@@ -4,10 +4,10 @@ source normalized_hydropathyscales.tcl
 #Filename needs to be csv
 
 proc blobTest {filename molID lMin H select {dictInput "Kyte-Doolittle"} } {
-	set blobulated [::blobulator::blobulateSelection top 4 .4 "all" $dictInput]
+	set blobulated [::blobulator::blobulateSelection $molID 4 .4 "all" $dictInput]
 
 	puts "TCL:$blobulated"
-	set file [open filename w]
+	set file [open $filename w]
 	set count 0
 	puts $file "Count, BlobNum"
 	foreach residue $blobulated {
