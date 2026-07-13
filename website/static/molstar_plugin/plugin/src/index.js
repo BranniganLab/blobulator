7485da26a737462c83c79fc5f5412cb9db9fb9ab
cpitman-11 <pitman.connor@rutgers.edu>
2026-07-13 12:49:25 -0400

    uploading compiled version

--- website/static/molstar_plugin/plugin/src/index.js
+++ website/static/molstar_plugin/plugin/src/index.js
@@ -212,7 +212,7 @@ function createPlugin(parent) {
                     setTimeout(function () {
                         createBlobRepresentation(plugin);
                     }, 1000);
-                    elementsArray = document.querySelectorAll('.mutatebox,#snp_id,#residue_type,#domain_threshold_user_box,#domain_threshold_user_slider,#cutoff_user_box,#cutoff_user_slider,.checkbox,#hydro_scales');
+                    elementsArray = document.querySelectorAll('.mutatebox,#snp_id,#residue_type,#domain_threshold_user_box,#domain_threshold_user_slider,#cutoff_user_box,#cutoff_user_slider,.checkbox,#hydro_scales,#preset_dropdown');
                     elementsArray.forEach(function (elem) {
                         elem.addEventListener('change', function () {
                             setTimeout(function () {
@@ -222,7 +222,6 @@ function createPlugin(parent) {
                     });
                     reset_button = document.querySelector('#reset_settings_button');
                     reset_button === null || reset_button === void 0 ? void 0 : reset_button.addEventListener('click', function () {
-                        console.log('reset button clicked!');
                         setTimeout(function () {
                             createBlobRepresentation(plugin);
                         }, 1000);
